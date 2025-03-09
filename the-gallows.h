72fc98b5b7f2466fcd050ba30401722de1c13be9
bbbgviff <ksyu.sl08@yandex.ru>
2025-03-09 14:10:00 +0300

    Added the first version of the code.

---
+++ the-gallows.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class hang
+{
+private:
+    int _Stad = 0;
+    static const int _MaxTry;
+    const std::vector<std::string> _HANGMAN = {
+        "\n\n\n\n\n\n=========" ,
+        "\n\n\n\n\n  |\n=========" ,
+        "\n\n\n\n  |\n  |\n=========" ,
+        "\n\n\n  |\n  |\n  |\n=========" ,
+        "\n\n  |\n  |\n  |\n  |\n=========" ,
+        "\n  |\n  |\n  |\n  |\n  |\n=========" ,
+        "  ____\n  |  |\n  |  O\n  | /|\\\n  |  |\n  | / \\\n  |\n========="
+    };
+public:
+    void NextStud(int tries);
+    static const int GetTry() {
+        return _MaxTry;
+    }
+};
